2bd6b6e043a5d16ca48e7c8fc411d50a19c46b11
Abraham Toriz <awonderfulcode@gmail.com>
2017-08-22 21:49:17 -0500

    upgrade version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Flask-Coralillo',
-    version='0.1.0',
+    version='0.1.1',
     url='http://categulario.github.io/flask_coralillo',
     license='MIT',
     author='Abraham Toriz Cruz',
